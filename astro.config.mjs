66829fca21d168fdb60ddf097dc5507d01637c1a
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-09-21 15:48:25 +0200

    about page quick fix for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  output: "server",
+  adapter: vercel(),
   integrations: [
     tailwind(),
     image({
