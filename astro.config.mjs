96da32f3aee156035ef8a7390962f089b40b5060
Jimy Marletta <84329916+jimymltta@users.noreply.github.com>
2023-09-26 17:32:44 +0200

    back to vercel static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 // import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  output: "server",
+  site: "https://agenceneodesign.com",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
